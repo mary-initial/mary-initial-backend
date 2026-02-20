fff584860aa95ad3a1237a65b913cafc1e88670d
Jakob Vase <jakobvase@gmail.com>
2026-02-20 17:10:53 +0100

    add a test

---
+++ backend/src/Program.test.cs
@@ -0,0 +1,34 @@
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Mary.Backend.Tests;
+
+public class GraphQLTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public GraphQLTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GreetingQuery_ReturnsHelloWorld()
+    {
+        var payload = new { query = "{ greeting { message } }" };
+
+        var response = await _client.PostAsJsonAsync("/api/graphql", payload);
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadFromJsonAsync<JsonElement>();
+
+        Assert.True(json.TryGetProperty("data", out var data));
+        Assert.True(data.TryGetProperty("greeting", out var greeting));
+        Assert.True(greeting.TryGetProperty("message", out var message));
+        Assert.Equal("Hello, World!", message.GetString());
+    }
+}
