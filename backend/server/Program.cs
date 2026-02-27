1480ae090db35701d554319be359454684bb273f
Jakob Vase <jakobvase@gmail.com>
2026-02-27 15:39:26 +0100

    add health endpoint and port override

--- backend/server/Program.cs
+++ backend/server/Program.cs
@@ -7,12 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 var schema = new Schema { Query = new Query() };
 builder.Services.AddGraphQL(b => b.AddSchema(schema).AddSystemTextJson());
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 var app = builder.Build();
+
 app.UseDeveloperExceptionPage();
+
+app.MapGet("/healthz", () => Results.Ok(new { status = true }));
+
 app.UseGraphQL("/api/graphql");
 app.UseGraphQLGraphiQL("/", new GraphQL.Server.Ui.GraphiQL.GraphiQLOptions
 {
     GraphQLEndPoint = "/api/graphql",         // url of GraphQL endpoint
     // SubscriptionsEndPoint = "/api/graphql",   // url of GraphQL endpoint
 });
+
 await app.RunAsync();
